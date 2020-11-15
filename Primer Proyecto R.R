8ddd720448d619c3bdffe73d77fb578d96c5b8c3
jose acevedo <jacevedol.1994@gmail.com>
2020-11-15 18:42:30 -0300

    Hice la primera tarea del curso de DataScience

--- Primer Proyecto R.R
+++ Primer Proyecto R.R
@@ -4,3 +4,6 @@ print("Editando otro poco en R para enviar a Git")
 
 print("no logro que esto se suba a git")
 print("Creo que ya logré vincular ambos programas")
+
+print("Hi, my name is José. I'm from Chile. Proof of this, is that I wrote in spanish above")
+print("This is the first homework, and now i'll take the screenshot. Que estés bien :)")
