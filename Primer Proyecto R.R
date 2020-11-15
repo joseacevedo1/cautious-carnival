d10a2d5f9f44f8139bd379f9f948631c33b4926f
jose acevedo <jacevedol.1994@gmail.com>
2020-11-15 16:44:42 -0300

    Intento vincular R con Git

---
+++ Primer Proyecto R.R
@@ -0,0 +1,6 @@
+print("Este archivo fue creado en Rstudio")
+print("Y ahora está guardado en GitHub")
+print("Editando otro poco en R para enviar a Git")
+
+print("no logro que esto se suba a git")
+
